ddf4cfadd7815c24df2402f4911e7592879471c4
Jon Guymon <gnarg@mycroft.(none)>
2009-06-11 20:42:55 -0400

    just comment toggle so far

---
+++ gnarg.el
@@ -0,0 +1,24 @@
+(defun comment-or-uncomment-line (&optional lines)
+  "Comment current line. Argument gives the number of lines
+forward to comment"
+  (interactive "P")
+  (comment-or-uncomment-region
+   (line-beginning-position)
+   (line-end-position lines)))
+
+(defun comment-or-uncomment-region-or-line (&optional lines)
+  "If the line or region is not a comment, comments region
+if mark is active, line otherwise. If the line or region
+is a comment, uncomment."
+  (interactive "P")
+  (if mark-active
+      (if (< (mark) (point))
+	  (comment-or-uncomment-region (mark) (point))
+	(comment-or-uncomment-region (point) (mark))
+	)
+    (comment-or-uncomment-line lines)))
+
+(global-set-key [(meta /)] 'comment-or-uncomment-region-or-line)
+(global-set-key [(meta \\)] 'hippie-expand)
+
+;(global-auto-revert t)
